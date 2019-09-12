4eedc5727a6ac5a0aee9290a44e4515f1201b8bc
lesliesage <leslie.sage@gmail.com>
2019-09-12 13:48:55 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      const newFriend = [
+        {
+          name: action.friend.name,
+          id: action.friend.id,
+          hometown: action.friend.hometown
+        }
+      ];
+      const newFriendArray = state.friends.concat(newFriend);
+      return { ...state, friends: newFriendArray };
+    case "REMOVE_FRIEND":
+      const filteredFriends = state.friends.filter(friend => {
+        return action.id !== friend.id;
+      });
+      return {
+        ...state,
+        friends: filteredFriends
+      };
+    default:
+      return state;
+  }
 }
