a88246ec97be839076b60df77cd36b377c5f4e5c
Trevahr Robertson <Trevahr@gmail.com>
2019-09-12 17:28:52 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
 export function manageFriends(state, action){
+    
+
+        switch (action.type) {
+            case 'ADD_FRIEND':
+                return {...state, friends:[...state.friends, action.friend]}
+            case 'REMOVE_FRIEND':
+
+                    let newState = state.friends.filter(friend => friend.id !== action.id)
+                return {...state, friends: newState}
+            default:
+                return state;
+        }
+
+
+
+
+
 }
